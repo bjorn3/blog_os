f2ec16dec4d9dce65aacfe3b5a4293f2f3b4bad3
Philipp Oppermann <dev@phil-opp.com>
2019-04-08 13:36:25 +0200

    Use conditional compilation for entry points

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,13 @@
 
 use core::panic::PanicInfo;
 
+#[cfg(target_os = "linux")]
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
+#[cfg(any(target_os = "windows", target_os = "macos"))]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     loop {}
